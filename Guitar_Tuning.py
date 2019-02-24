27a0a1bc2b1fcf53aa0b706c1bfe9784e69e1d5d
MasterKitsune <reef.kitaeff@gmail.com>
2019-02-24 20:10:06 +0800

    Strings are now almost correctly calculated, other than being in reverse! Added a unique set to show strings with unique artists

--- Guitar_Tuning.py
+++ Guitar_Tuning.py
@@ -103,6 +103,7 @@ def dif(tun1,tun2): # takes difference between each string of two tunings.
 # We need to fix the tuning difference by considering different keys.
 # Let a(i) be the difference of the ith string.
 def dif_fix(dif_tun):  # Want min total difference. i.e. min[(a(1) + n) + (a(2) + n) + ... + (a(6) + n)]
+    '''
     k_sum = sum(dif_tun)
 
     k = k_sum % 6       # want min|k|
@@ -112,32 +113,39 @@ def dif_fix(dif_tun):  # Want min total difference. i.e. min[(a(1) + n) + (a(2) 
     n = int((k-k_sum)/6)         # do some algebra from above to get this expression
     dif_numpy = numpy.array(dif_tun)    # this allows a constant to be added to the whole list
     return dif_numpy + n
-
+    '''
+    #dif_numpy = numpy.array(dif_tun)
+    maximum = max(dif_tun)
+    fix = []
+    for i in dif_tun:
+        fix.append(i-maximum)
+    return fix
 '''
 TO DO!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 '''
 def dif_count(dif_tun, count): # Want max of 'count' number of strings to change. So we must find the amount of strings which have the same difference
+    '''
     if dif_tun.count(max(set(dif_tun), key = dif_tun.count)) >= 6 - count:
         return True
     else:
         return False
+    '''
+    if dif_tun.count(0) >= 6-count:
+        return True
+    else:
+        return False
 
-
-'''
-def id(tuning):
-    global id_num
-    global id_tunings
-    print(tuning)
-    id_tunings[tuning] = id_num
-    id_num+=1
-'''
-def checker(dif_tun, n): # checks if a difference tuning is acceptable, i.e. max variation of n
+def checker(dif_tun, n): # checks if a difference tuning is acceptable, i.e. max decrease of n
     bool = True
+    '''
     for i in dif_tun:
         if (i < -n) or (i > n):
             bool = False
+    '''
+    if min(dif_tun) < -n:
+        bool = False
+    ''''''
     return bool
-
 
 def good_set(tunings, n, count): # find 'good_set's, where a 'good_set' is a list of tunings where its not hard to change between them
     #MUST INPUT FORMATED TUNINGS (but not fixed)
@@ -153,50 +161,70 @@ def good_set(tunings, n, count): # find 'good_set's, where a 'good_set' is a lis
         for j in range(length):
             if i != j:              # better program: i>j (imagine a table of i against j to see where repetition occurs, and hence why this is best)!!!!!!!!!!!!!!!!!!!!
                 #differences[(i,j)] = dif_fix((i,j))
-                diff = dif(tunings[i], tunings[j])
-                if checker(dif_fix(diff), n):
+                diff = dif_fix(dif(tunings[i], tunings[j]))
+                if checker(diff, n):
                     if dif_count(diff, count):
                         good_set.append(j)
         good_sets.append(good_set)
     return good_sets
+
 def string_start(good_set):
     for main_tuning in range(length):
         variations = good_set[main_tuning]
         if variations != []:
-            global_strings.append(string_inf([main_tuning]))
+            global_strings.extend(string_inf([main_tuning]))
+
 def string_inf(string):
     total_string = []
-    vars = good_sets[string[-1]]
-    if vars == []:
+    vars = good_sets[string[-1]] #variations
+    if vars == []: #deals with no variations. loop closes automatically
         total_string.append(string)
+
     for variation in vars:
-        new_string = []
-        for q in string:
-            new_string.append(q)
+        new_string = string[:] #defines new string, i.e. old string + variation (after it's been checked that it's not already done)
         new_string.append(variation)
-        if variation not in string:
-            for i in string_inf(new_string):
-                total_string.append(i)
-        else:
+
+        if variation not in string: #if no loop, then we can let recursion occur
+            bool = True
+            # finishes the string if the rest has been done before
+            for done_string in used_strings:
+                if done_string[0] == variation:
+                    bool = False
+                    new_new_string = new_string[:-1]
+
+                    for i in done_string: #considers cyclic strings, i.e. only adds part of the done_string to
+                        if i in new_new_string:
+                            new_new_string.append(i)
+                            break
+                        else:
+                            new_new_string.append(i)
+
+                    for i in total_string:#considers if strings in total_string are already made
+                        if new_new_string not in total_string:
+                            total_string.append(new_new_string)
+            for done_string in global_strings:
+                if done_string[0] == variation:
+                    bool = False
+                    new_new_string = new_string[:-1]
+
+                    for i in done_string: #considers cyclic strings, i.e. only adds part of the done_string to
+                        if i in new_new_string:
+                            new_new_string.append(i)
+                            break
+                        else:
+                            new_new_string.append(i)
+                    for i in total_string:#considers if strings in total_string are already made
+                        if new_new_string not in total_string:
+                            total_string.append(new_new_string)
+                            used_strings.append(done_string)
+                            global_strings.remove(done_string)
+            if bool:
+                total_string.extend(string_inf(new_string))
+        else: # if loop, then string closes
             total_string.append(new_string)
 
     return total_string
 
-    '''
-    total_strings = []
-    for var in variations:
-        new_string = string
-        new_string.append(var)  # extends string to have variation/main tuning i
-        if var not in string: # prevents loops
-            for collected_string in string_inf(good_sets[var], new_string): # finds all string variations
-                total_strings.append(collected_string)
-                # repetition occurs as we let the main tunings turn into variations for their main tuning
-        else:
-            global_strings.append(string)
-    if total_strings==[]:
-        global_strings.append(string) # stores all strings together
-    return total_strings # returns all string variations of i
-    '''
 
 def display(id):
     tuning = ""
@@ -235,11 +263,27 @@ if "__main__" == __name__:
     string_start(good_sets)
     print(global_strings)
     for i in global_strings:
-        for k in i:
-            count += 1
-            results += "Set " + str(count) + ":\n"
-            for j in i:
-                results += display(j)
-            results+="\n"
-    file = open('Guitar Tunings.txt','w')
+        count += 1
+        results += "Set " + str(count) + ":\n"
+        for j in i:
+            results += display(j)
+        results+="\n"
+
+    results += "\n\n\n"
+    results += "Unique Strings:\n"
+    count = 0
+    unique_strings = []
+    for i in global_strings:
+        artists = []
+        for j in i:
+            artists.append(pull[j][1])
+        if artists.count(max(set(artists), key = artists.count)) == 1:
+            unique_strings.append(i)
+    for i in unique_strings:
+        count += 1
+        results += "Unique set " + str(count) + ":\n"
+        for j in i:
+            results += display(j)
+        results+="\n"
+    file = open('Guitar Tunings.txt', 'w')
     file.write(results)
